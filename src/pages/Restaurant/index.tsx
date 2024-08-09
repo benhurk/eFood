a41635862d08f7b8abe1650cf0640caefeb3d840
Ben Hur <benhurk2801@gmail.com>
2024-08-09 14:11:03 -0300

    Modal adicionado

--- src/pages/Restaurant/index.tsx
+++ src/pages/Restaurant/index.tsx
@@ -8,6 +8,7 @@ import RestaurantBanner from "../../components/RestaurantBanner";
 import ProductCard from "../../components/ProductCard";
 
 import productImage from '../../assets/pizza.png';
+import Modal from '../../components/Modal'
 
 export default function Restaurant() {
     return (
@@ -48,6 +49,7 @@ export default function Restaurant() {
                             </li>
                         </ProductList>
                     </div>
+                    <Modal name="Pizza Marguerita" image={productImage} info='A pizza Margherita é uma pizza clássica da culinária italiana, reconhecida por sua simplicidade e sabor inigualável. Ela é feita com uma base de massa fina e crocante, coberta com molho de tomate fresco, queijo mussarela de alta qualidade, manjericão fresco e azeite de oliva extra-virgem. A combinação de sabores é perfeita, com o molho de tomate suculento e ligeiramente ácido, o queijo derretido e cremoso e as folhas de manjericão frescas, que adicionam um toque de sabor herbáceo. É uma pizza simples, mas deliciosa, que agrada a todos os paladares e é uma ótima opção para qualquer ocasião. Serve: de 2 a 3 pessoas' price='R$60,90' />
                 </section>
             </main>
         </>
