dfbcceba021474c82f458dda6d8776007af676c5
Ben Hur <benhurk2801@gmail.com>
2024-08-10 15:20:07 -0300

    Adicionada funcionalidade para fechar e a condição para renderização

--- src/components/Modal/index.tsx
+++ src/components/Modal/index.tsx
@@ -1,6 +1,8 @@
 import Button from "../Button";
 import { CloseBtn, ModalContainer, ModalContent } from "./styled";
 import closeIcon from '../../assets/closeX.svg';
+import { useContext } from "react";
+import { ModalContext } from "../../contexts/ModalContext";
 
 type Props = {
     image: string;
@@ -10,19 +12,26 @@ type Props = {
 }
 
 export default function Modal({name, image, info, price}: Props) {
+    const { isOpen, setIsOpen } = useContext(ModalContext);
+
     return (
-        <ModalContainer>
-            <div className="container">
-                <ModalContent>
-                    <img src={image} />
-                    <div className="modal__inner">
-                        <h3>{name}</h3>
-                        <p>{info}</p>
-                        <Button color="cream" width="fit-content">{`Adicionar ao carrinho - ${price}`}</Button>
+        <>
+            {
+                isOpen &&
+                <ModalContainer onClick={() => setIsOpen(false)}>
+                    <div className="container">
+                        <ModalContent>
+                            <img src={image} />
+                            <div className="modal__inner">
+                                <h3>{name}</h3>
+                                <p>{info}</p>
+                                <Button color="cream" width="fit-content">{`Adicionar ao carrinho - ${price}`}</Button>
+                            </div>
+                            <CloseBtn onClick={() => setIsOpen(false)}><img src={closeIcon} /></CloseBtn>
+                        </ModalContent>
                     </div>
-                    <CloseBtn><img src={closeIcon}></img></CloseBtn>
-                </ModalContent>
-            </div>
-        </ModalContainer>
+                </ModalContainer>
+            }
+        </>
     )
 }
