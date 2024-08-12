0edbf6f99d08bfda48812d64b33baae364edbd3a
Ben Hur <benhurk2801@gmail.com>
2024-08-12 19:44:03 -0300

    Arrumado a imagem não aparecer

--- src/components/SiteTitle/index.tsx
+++ src/components/SiteTitle/index.tsx
@@ -1,7 +1,9 @@
+import logo from '../../assets/logo.svg';
+
 export default function SiteTitle() {
     return (
         <h1>
-            <img src="logo.svg" alt="eFood" />
+            <img src={logo} alt="eFood" />
         </h1>
     )
 }
