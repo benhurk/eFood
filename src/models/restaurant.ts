c3d70d5afe734ca25ef8f66096cb4b2850544ffa
Ben Hur <benhurk2801@gmail.com>
2024-08-12 19:41:30 -0300

    Criado um type para os restaurantes

---
+++ src/models/restaurant.ts
@@ -0,0 +1,19 @@
+export type ProductType = {
+    foto: string;
+    preco: number;
+    id: number;
+    nome: string;
+    descricao: string;
+    porcao: string;
+}
+
+export type RestaurantType = {
+    id: number;
+    titulo: string;
+    destacado: boolean;
+    tipo: string;
+    avaliacao: number;
+    descricao: string;
+    capa: string;
+    cardapio: ProductType[];
+}
