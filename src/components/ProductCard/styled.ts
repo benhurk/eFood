b06559bcc5b5a86a6955f16994033e3bf36fcfd6
Ben Hur <benhurk2801@gmail.com>
2024-08-13 13:51:56 -0300

    Limitando o tamanho da descrição no card

--- src/components/ProductCard/styled.ts
+++ src/components/ProductCard/styled.ts
@@ -5,14 +5,22 @@ export const ProductContainer = styled.div`
     padding: 8px;
     background-color: ${colors.red};
     width: 100%;
+    height: 360px;
 `
 
 export const ProductImg = styled.div`
-    height: 168px;
+    height: 50%;
     background-size: cover;
     background-repeat: no-repeat;
     background-position: center center;
     cursor: pointer;
+`
+
+export const ProductBody = styled.div`
+    height: 50%;
+    display: flex;
+    flex-direction: column;
+    justify-content: space-between;
 `
 
 export const ProductText = styled.div`
@@ -28,5 +36,11 @@ export const ProductText = styled.div`
     .product_description {
         font-size: 14px;
         line-height: 22px;
+        max-height: 100px;
+        overflow-y: hidden;
+        background: linear-gradient(to bottom, ${colors.cream}, 80%, ${colors.red});
+        -webkit-background-clip: text;
+        background-clip: text;
+        -webkit-text-fill-color: transparent;
     }
 `
