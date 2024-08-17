6e690e47d43bf81890d71d23e4386d0027c845b1
Ben Hur <benhurk2801@gmail.com>
2024-08-17 16:00:40 -0300

    fix: Arrumado o ícone do site não aparecer corretamente

--- src/components/Footer/index.tsx
+++ src/components/Footer/index.tsx
@@ -1,3 +1,4 @@
+import logo from '../../assets/logo.svg';
 import facebookIcon from '../../assets/facebook-icon.svg';
 import instagramIcon from '../../assets/instagram-icon.svg';
 import twitterIcon from '../../assets/twitter-icon.svg';
@@ -7,7 +8,7 @@ import { FooterContainer, FooterText, SocialLinks } from "./styled"
 export default function Footer() {
     return (
         <FooterContainer>
-            <img src='logo.svg' alt='eFood'></img>
+            <img src={logo} alt='eFood'></img>
             <SocialLinks>
                 <li>
                     <a href='#'><img src={instagramIcon} /></a>
