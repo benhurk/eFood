51d9d65c7b88e9a87b824d5274c486f4881e452b
Ben Hur <benhurk2801@gmail.com>
2024-08-23 14:17:35 -0300

    feat: Renderiza o título e o componente Form dinâmicamente

--- src/components/Sidebar/index.tsx
+++ src/components/Sidebar/index.tsx
@@ -2,20 +2,38 @@ import { useDispatch, useSelector } from "react-redux";
 import { RootReducer } from "../../store";
 import { toggleSidebar } from "../../store/reducers/sidebar";
 
+import getTotalPrice from "../../utils/getTotalPrice";
+import formatPrice from "../../utils/formatPrice";
+
 import { SidebarContainer, SidebarContent, SidebarOverlay } from "./styled";
 import Cart from "../Cart";
 import CloseButton from "../CloseButton";
-import AddressForm from "../AddressForm";
+import Form from "../Form";
+
+export type Content = 'Cart' | 'Address' | 'Payment' | 'Success';
 
 export default function Sidebar() {
+    const { items } = useSelector((state: RootReducer) => state.cart);
     const { content, isOpen } = useSelector((state: RootReducer) => state.sidebar);
     const dispatch = useDispatch();
 
     function getContent() {
         switch (content) {
             case "Cart": return <Cart /> 
-            case "Address": return <AddressForm />
+            case "Address":
+                return (
+                    <>
+                    <h2>Endereço</h2> 
+                    <Form content='Address' />
+                    </>
+                )
             case "Payment":
+                return (
+                    <>
+                    <h2>{`Pagamento - Valor a pagar: ${formatPrice(getTotalPrice(items))}`}</h2> 
+                    <Form content='Payment' />
+                    </>
+                )
             case "Success":
         }
     }
