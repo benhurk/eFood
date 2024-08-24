a04de2d7df39b59acff053427d4e0abf49ade4c2
Ben Hur <benhurk2801@gmail.com>
2024-08-24 14:57:48 -0300

    refactor: Adicionada prop para o tipo do botão

--- src/components/Button/index.tsx
+++ src/components/Button/index.tsx
@@ -2,14 +2,15 @@ import Btn from "./styled"
 
 export type Props = {
     children: JSX.Element | string;
+    type?: 'Button' | 'Submit';
     color: 'red' | 'cream';
     width: '100%' | 'fit-content';
     onClick?: () => any;
     disabled?: boolean;
 }
 
-export default function Button({children, color, width, onClick, disabled}: Props) {
+export default function Button({type, children, color, width, onClick, disabled}: Props) {
     return (
-        <Btn color={color} width={width} onClick={onClick} disabled={disabled}>{children}</Btn>
+        <Btn type={type} color={color} width={width} onClick={onClick} disabled={disabled}>{children}</Btn>
     )
 }
