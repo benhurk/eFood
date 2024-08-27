85a9b5ffbf96a6a06dbe368337e3b6bb6e383659
Ben Hur <benhurk2801@gmail.com>
2024-08-27 13:42:22 -0300

    feat: Criado o type do pedido

---
+++ src/models/order.ts
@@ -0,0 +1,35 @@
+type OrderProducts = {
+    id: number;
+    price: number;
+}
+
+type Order = {
+    products: OrderProducts[];
+
+    delivery: {
+        receiver: string;
+
+        address: {
+            description: string;
+            city: string;
+            zipCode: string;
+            number: number;
+            complement?: string;
+        }
+    }
+
+    payment: {
+        card: {
+            name: string;
+            number: string;
+            code: number;
+
+            expires: {
+                month: number;
+                year: number;
+            }
+        }
+    }
+}
+
+export default Order;
