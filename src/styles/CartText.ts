8fd0f749674d482daf427a19774d31a1bd422886
Ben Hur <benhurk2801@gmail.com>
2024-09-02 14:06:31 -0300

    feat: Estilos para o ícone

--- src/styles/CartText.ts
+++ src/styles/CartText.ts
@@ -2,12 +2,19 @@ import styled from "styled-components";
 import { colors } from "../globalStyles";
 
 const CartText = styled.button`
+    display: flex;
+    gap: 4px;
     font-size: 18px;
     color: ${colors.red};
     font-weight: 900;
     background-color: transparent;
     border: none;
+    align-items: center;
     cursor: pointer;
+
+    img {
+        width: 24px;
+    }
 `
 
 export default CartText;
